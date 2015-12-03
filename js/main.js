60be8bd2d09e28f5880508456583a6a2e398dd76
werckerbot <pleasemailus@wercker.com>
2015-12-03 21:01:57 +0000

    deploy from bartels

---
+++ js/main.js
@@ -0,0 +1,109 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+    }).catch(function(err) {
+        // registration failed :(
+        console.log('ServiceWorker registration failed: ', err);
+    });
+}
+
+console.log("main.js loaded");
+
+// save templates
+var templateCache = {};
+
+// setup mustache templating
+var parseTags = new Array();
+parseTags.push("[[");
+parseTags.push("]]");
+
+/**
+ * initiate a template and add it to the cache
+ * @param id id from the template
+ */
+var initTemplate = function(id) {
+    var tpl = document.querySelector('#' + id);
+    var tplHtml = tpl.innerHTML;
+
+    Mustache.parse(tplHtml, parseTags);
+    templateCache[id] = tplHtml;
+}
+
+initTemplate('tplModList');
+
+
+var loadedViaServiceWorker = false;
+
+/**
+ * load the swproxy-mods from own Server (/swproxy/mods)
+ * @returns {Promise}
+ */
+var loadMods = function() {
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', '/swproxy/mods', true);
+        xhr.onload = function(e) {
+            console.log('Server response status: ', this.status);
+            if (this.status == 200) {
+                loadedViaServiceWorker = true;
+                resolve(JSON.parse(this.response));
+            }
+            reject();
+        };
+        xhr.send();
+    });
+};
+
+/**
+ * Because npm set no CORS Header, we need to load the data from a proxy
+ * @returns {Promise}
+ */
+var loadModsViaProxy = function() {
+    console.log("Mods not loaded from ServiceWorker!");
+    return new Promise(function(resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', 'https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/-/_view/byKeyword?startkey=[%22swproxy-mod%22]&endkey=[%22swproxy-mod%22,%7B%7D]&group_level=3', true);
+        xhr.onload = function(e) {
+            console.log('Server response status (via proxy): ', this.status);
+            if (this.status == 200) {
+                resolve(JSON.parse(this.response));
+            }
+            reject();
+        };
+        xhr.send();
+    });
+};
+
+/**
+ * shows the mods on the webpage
+ * @param mods
+ */
+var showMods = function (mods) {
+    var rows = [];
+    for (var i = 0; i < mods.rows.length; i++) {
+        rows.push({
+            name: mods.rows[i].key[1], // package name
+            description: mods.rows[i].key[2], // package description
+            href: 'https://www.npmjs.com/package/' + mods.rows[i].key[1] // link to view package on npm
+        });
+    }
+
+    var rendered = Mustache.render(templateCache['tplModList'], {rows: rows});
+    var container = document.querySelector('#resultContainer');
+    container.innerHTML = rendered;
+
+    document.querySelector(loadedViaServiceWorker ? '.badge-sw' : '.badge-no-sw').setAttribute('style', 'display: block');
+};
+
+/**
+ * shows a error on the webpage
+ */
+var showNetworkError = function () {
+    console.log('loading mods failed with network error');
+};
+
+// load mods from own server, if it fail, try to load via proxy
+loadMods()
+    .then(undefined, loadModsViaProxy)
+    .then(showMods, showNetworkError);
---
+++ sw.js
@@ -0,0 +1,43 @@
+
+importScripts('./js/swproxy.js');
+importScripts('./js/swproxy-mod-rewrite.js');
+
+var proxy = new SwProxy(self);
+proxy.registerMod(ModRewrite);
+
+/*
+proxy.registerMod({
+    factoryMethodName: () => 'rewriteRule',
+    factoryMethod: (swproxy) => {
+        return (url, rule, modifiers) => {
+            swproxy.addFetchRule({
+                match: (event) => {
+                    return url.test(event.request.url);
+                },
+                execute: (originalEvent, event) => {
+                    console.log('execute rule for request: ', rule, originalEvent, event);
+                    return new Promise((resolve) => {
+                        var matches = url.exec(event.request.url);
+                        var destUrl = rule;
+
+                        matches.forEach((match, i) => {
+                            destUrl = destUrl.replace('$' + i, match);
+                        });
+
+                        event.request.url = destUrl;
+                        resolve(event);
+                    });
+                }
+            });
+        }
+    }
+});
+*/
+
+// proxy.rewriteRule("asd", "Regex should match", "Modifiers");
+proxy.rewriteRule(new RegExp('.*/swproxy/mods$', ''), 'https://cors-anywhere.herokuapp.com/https://registry.npmjs.org/-/_view/byKeyword?startkey=[%22swproxy-mod%22]&endkey=[%22swproxy-mod%22,%7B%7D]&group_level=3', {});
+
+
+// proxy.mockRule(new RegExp('.*/rest/foo'), {
+//    foo: 'bar'
+// }, {});
